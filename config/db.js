ec8a31d3f23738dcc709ebec73f690ac94a52b22
stygian-96 <blaze.ill.03@gmail.com>
2025-03-02 23:38:36 +0530

    Added a log to debug deployment issue

--- config/db.js
+++ config/db.js
@@ -9,6 +9,14 @@ const pool = mysql.createPool({
   connectionLimit: 10
 });
 
+console.log("DB Connection Details (Railway):", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS ? "***" : "Not Set",
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
 pool.getConnection((err, connection) => {
   if (err) {
     console.error('Database connection failed:', err);
